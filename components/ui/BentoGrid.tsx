516d478eb342e6ea75f642c1a554c4756f62923a
Marzx8 <168499078+Marzx8@users.noreply.github.com>
2024-05-17 18:19:35 +0100

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "contact@olivia.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
